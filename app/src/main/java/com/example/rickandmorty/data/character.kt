defdb79447f60320907bffe279bd765de57ecb8c
Renz Uriel <rdeypalubos_210000001722@uic.edu.ph>
2025-02-07 15:41:18 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/rickandmorty/data/character.kt
@@ -0,0 +1,12 @@
+package com.example.rickandmorty.data
+
+data class character(
+    val nameRes: Int,
+    val statusRes: Int,
+    val speciesRes: Int,
+    val type: String,
+    val gender: String,
+    val origin: String,
+    val location: String,
+    val imageRes: Int
+)
---
+++ app/src/main/java/com/example/rickandmorty/ui/theme/CharacterListScreen.kt
@@ -0,0 +1,49 @@
+package com.example.rickandmorty
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.rickandmorty.data.characters
+
+@Composable
+fun CharacterListScreen() {
+    Column {
+        Header()
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            items(characters) { character ->
+                CharacterCard(character)
+            }
+        }
+    }
+}
+
+@Composable
+fun Header() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .background(Color(0xFFFFF59D)),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.rick_and_morty_logo),
+            contentDescription = "Header Image",
+            modifier = Modifier.size(130.dp)
+        )
+    }
+}
